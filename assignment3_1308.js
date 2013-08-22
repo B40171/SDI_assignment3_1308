54f7d647ef6c462e7e4fa1b5fb1a8b88fdfce60f
bnlevan <bnlevan@fullsail.edu>
2013-08-22 00:58:10 -0400

    added method procedure.

--- assignment3_1308.js
+++ assignment3_1308.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+//Brent Levan
+//SDI 1308
+//Project 3
+// Methods,objects,json
+
+
+var newGame = {
+    
+    "type1": "single player",
+    "type2": "multiplayer",
+    "type3": "online",
+    "selection": function(gameType) {
+        
+        newGame = gameType
+        
+        console.log("You have chosen " + newGame + ".");
+        
+    }
+    
+}
+    newGame.selection(newGame.type2);
